88e930830eaa28856bfe33f62275c7cb2b346707
marin <marin.m.11087@icloud.com>
2024-12-23 10:38:20 +0900

    "形問題出題画面"

--- adventer_app/lib/EducationModeScreen/ShapeEducationScreen.dart
+++ adventer_app/lib/EducationModeScreen/ShapeEducationScreen.dart
@@ -1,7 +1,73 @@
 import 'package:flutter/material.dart';
-import 'EducationCorrectScreen.dart';
-import 'EducationIncorrectScreen.dart';
+import 'dart:convert'; // JSONデータを扱うため
+import 'package:http/http.dart' as http;
+import 'EducationCorrectScreen.dart'; //正解画面
+import 'EducationIncorrectScreen.dart'; //不正解画面
 import 'EducationModeScreen.dart';
+
+// questionのデータモデル
+class Question {
+  final String question_id;
+  final String questiontype_id;
+  final String question_theme;
+  final String question_answer;
+  final String question_content;
+  final Map<String, String> options;
+
+  Question({
+    required this.question_id,
+    required this.questiontype_id,
+    required this.question_theme,
+    required this.question_answer,
+    required this.question_content,
+    required this.options,
+  });
+
+  // JSONをQuestionオブジェクトに変換
+  factory Question.fromJson(Map<String, dynamic> json) {
+    return Question(
+      question_id: json['question_id'],
+      questiontype_id: json['questiontype_id'],
+      question_theme: json['question_theme'],
+      question_answer: json['question_answer'],
+      question_content: json['question_content'],
+      options: json['options'] != null && json['options'].isNotEmpty
+          ? Map<String, String>.from(json['options'])
+          : {'No options available': ''}, // デフォルト値
+    );
+  }
+}
+
+// APIリクエストを送信して、問題を取得するメソッド
+Future<Question?> fetchQuestion(String questiontypeId) async {
+  final response = await http.get(
+    Uri.parse(
+        'http://10.24.110.65:8080/random-text-question?questiontype_id=$questiontypeId'),
+  );
+
+  if (response.statusCode == 200) {
+    return Question.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load question');
+  }
+}
+
+Future<String> submitAnswer(String questionId, String selectedAnswer) async {
+  final response = await http.post(
+    Uri.parse('http://10.24.110.65:8080/submit-answer'),
+    headers: {'Content-Type': 'application/json'},
+    body: jsonEncode({
+      'questionId': questionId,
+      'answer': selectedAnswer,
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    return response.body; // "correct" または "incorrect"
+  } else {
+    throw Exception('Failed to submit answer');
+  }
+}
 
 // 四角いボタンを定義
 class RectangularButton extends StatelessWidget {
@@ -56,11 +122,23 @@ class RectangularButton extends StatelessWidget {
   }
 }
 
-// 形問題出題画面
-class ShapeEducationScreen extends StatelessWidget {
+// 文字問題出題画面
+class ShapeEducationScreen extends StatefulWidget {
   const ShapeEducationScreen({super.key});
 
-  // ポップアップダイアログを表示する関数
+  @override
+  _ShapeEducationScreen createState() => _ShapeEducationScreen();
+}
+
+class _ShapeEducationScreen extends State<ShapeEducationScreen> {
+  late Future<Question?> questionFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    questionFuture = fetchQuestion("KMS001"); // questiontypeIdを指定
+  }
+
   void _showQuitDialog(BuildContext context) {
     showDialog(
       context: context,
@@ -78,7 +156,10 @@ class ShapeEducationScreen extends StatelessWidget {
             TextButton(
               onPressed: () {
                 Navigator.pop(context); // ダイアログを閉じて、問題一覧画面に戻る
-                Navigator.push(context, MaterialPageRoute(builder: (_) => const EducationModeScreen()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => const EducationModeScreen()));
               },
               child: const Text('やめる'),
             ),
@@ -88,17 +169,34 @@ class ShapeEducationScreen extends StatelessWidget {
     );
   }
 
+  // ユーザーが答えを選んだときに呼び出すメソッド
+  void _handleAnswerSubmission(
+      String selectedAnswer, Question question, BuildContext context) async {
+    final result = await submitAnswer(question.question_id, selectedAnswer);
+    if (result == "correct") {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) => const EducationCorrectScreen()));
+    } else {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) => const EducationIncorrectScreen()));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false, // 戻るボタンを非表示にする
-        backgroundColor: const Color.fromARGB(141, 57, 154, 0), 
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color.fromARGB(141, 57, 154, 0),
         elevation: 0,
         title: const Text(
-          'かたちもんだい',
+          'もじもんだい',
           style: TextStyle(
             color: Colors.white,
             fontSize: 22,
@@ -108,153 +206,157 @@ class ShapeEducationScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      backgroundColor: Colors.white, // 背景を白に統一
-      body: Stack(
-        children: [
-          // 上部のソフトな装飾
-          Positioned(
-            top: -50,
-            left: -50,
-            child: Container(
-              width: 150,
-              height: 150,
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(50, 255, 182, 193), // 薄いピンク
-                shape: BoxShape.circle,
-              ),
-            ),
-          ),
-          // 下部のソフトな装飾
-          Positioned(
-            bottom: -50,
-            right: -50,
-            child: Container(
-              width: 200,
-              height: 200,
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(50, 173, 216, 230), // 薄い水色
-                shape: BoxShape.circle,
-              ),
-            ),
-          ),
-          // 問題中断ボタン（左下）
-          Positioned(
-            bottom: 30,
-            left: 10,
-            child: TextButton(
-              onPressed: () {
-                _showQuitDialog(context); // ダイアログを表示
-              },
-              style: TextButton.styleFrom(
-                backgroundColor: const Color.fromARGB(141, 57, 154, 0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20), // 角丸
-                ),
-                padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 25),
-              ),
-              child: const Text(
-                'やめる',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white, // テキストカラー（白）
-                  fontFamily: 'Comic Sans MS', // フォント
-                ),
-              ),
-            ),
-          ),
-          // 問題テキスト
-          Positioned(
-            top: screenSize.height * 0.15,
-            left: 0,
-            right: 0,
-            child: Column(
+      backgroundColor: Colors.white,
+      body: FutureBuilder<Question?>(
+        future: questionFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final question = snapshot.data!;
+            return Stack(
               children: [
-                const Text(
-                  'このかたちと\nおなじかたちをみつけよう！',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    fontFamily: 'Comic Sans MS',
+                // 背景装飾
+                Positioned(
+                  top: -50,
+                  left: -50,
+                  child: Container(
+                    width: 150,
+                    height: 150,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(50, 255, 182, 193),
+                      shape: BoxShape.circle,
+                    ),
                   ),
                 ),
-                const SizedBox(height: 60),
-                // 問題の丸
-                Container(
-                  width: 160,
-                  height: 160,
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 154, 208, 255),
-                    shape: BoxShape.circle,
+                Positioned(
+                  bottom: -50,
+                  right: -50,
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(50, 173, 216, 230),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                // 問題中断ボタン（左下）
+                Positioned(
+                  bottom: 30,
+                  left: 10,
+                  child: TextButton(
+                    onPressed: () {
+                      _showQuitDialog(context); // ダイアログを表示
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(141, 57, 154, 0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20), // 角丸
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 15, horizontal: 25),
+                    ),
+                    child: const Text(
+                      'やめる',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white, // テキストカラー（白）
+                        fontFamily: 'Comic Sans MS', // フォント
+                      ),
+                    ),
+                  ),
+                ),
+                // 問題テキストと選択肢
+                Positioned(
+                  top: screenSize.height * 0.15,
+                  left: 0,
+                  right: 0,
+                  child: Column(
+                    children: [
+                      const Text(
+                        'このもじと\nおなじもじをみつけよう！',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontFamily: 'Comic Sans MS',
+                        ),
+                      ),
+                      const SizedBox(height: 60),
+                      Container(
+                        width: 160,
+                        height: 160,
+                        decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 154, 208, 255),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(
+                          child: Text(
+                            question.question_theme, // question_theme（問題内容)を表示
+                            style: const TextStyle(fontSize: 40),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // 選択肢ボタンエリア
+                Positioned(
+                  bottom: screenSize.height * 0.20,
+                  left: 0,
+                  right: 0,
+                  child: Column(
+                    children: [
+                      for (int i = 0; i < question.options.length; i += 2)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            if (i < question.options.length)
+                              RectangularButton(
+                                text: question.options.keys.toList()[i], //選択肢表示
+                                buttonColor:
+                                    const Color.fromARGB(255, 250, 240, 230),
+                                textColor: Colors.black,
+                                width: screenSize.width * 0.4,
+                                height: 70,
+                                onPressed: () {
+                                  final selectedAnswer =
+                                      question.options.keys.toList()[i];
+                                  _handleAnswerSubmission(selectedAnswer,
+                                      question, context); // 修正箇所
+                                },
+                              ),
+                            if (i + 1 < question.options.length)
+                              RectangularButton(
+                                text: question.options.keys.toList()[i + 1],
+                                buttonColor:
+                                    const Color.fromARGB(255, 250, 240, 230),
+                                textColor: Colors.black,
+                                width: screenSize.width * 0.4,
+                                height: 70,
+                                onPressed: () {
+                                  final selectedAnswer =
+                                      question.options.keys.toList()[i + 1];
+                                  _handleAnswerSubmission(selectedAnswer,
+                                      question, context); // 修正箇所
+                                },
+                              ),
+                          ],
+                        ),
+                    ],
                   ),
                 ),
               ],
-            ),
-          ),
-          // ボタンエリア
-          Positioned(
-            bottom: screenSize.height * 0.15,
-            left: 0,
-            right: 0,
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    RectangularButton(
-                      text: 'A.まる',
-                      buttonColor: const Color.fromARGB(255, 250, 240, 230),
-                      textColor: Colors.black,
-                      width: screenSize.width * 0.4,
-                      height: 70,
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (_) => const EducationCorrectScreen()));
-                      },
-                    ),
-                    RectangularButton(
-                      text: 'B.しかく',
-                      buttonColor: const Color.fromARGB(255, 250, 240, 230),
-                      textColor: Colors.black,
-                      width: screenSize.width * 0.4,
-                      height: 70,
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (_) => const EducationIncorrectScreen()));
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    RectangularButton(
-                      text: 'C.さんかく',
-                      buttonColor: const Color.fromARGB(255, 250, 240, 230),
-                      textColor: Colors.black,
-                      width: screenSize.width * 0.4,
-                      height: 70,
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (_) => const EducationIncorrectScreen()));
-                      },
-                    ),
-                    RectangularButton(
-                      text: 'D.ほし',
-                      buttonColor: const Color.fromARGB(255, 250, 240, 230),
-                      textColor: Colors.black,
-                      width: screenSize.width * 0.4,
-                      height: 70,
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (_) => const EducationIncorrectScreen()));
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+            );
+          } else {
+            return const Center(child: Text('No data available.'));
+          }
+        },
       ),
     );
   }
